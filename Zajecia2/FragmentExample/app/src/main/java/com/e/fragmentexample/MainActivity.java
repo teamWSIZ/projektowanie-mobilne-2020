99d5c930f290e51310b0b68e223bf242cc80b75a
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-03-28 14:28:27 +0100

    cd..

--- Zajecia2/FragmentExample/app/src/main/java/com/e/fragmentexample/MainActivity.java
+++ Zajecia2/FragmentExample/app/src/main/java/com/e/fragmentexample/MainActivity.java
@@ -2,20 +2,40 @@ package com.e.fragmentexample;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 public class MainActivity extends AppCompatActivity {
+    class MyFragmentAdapter extends FragmentStatePagerAdapter{
+
+        public MyFragmentAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return new BlankFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fm.beginTransaction();
-        fragmentTransaction.replace(R.id.main_layout,new BlankFragment());
-        fragmentTransaction.commit();
+        //<androidx.viewpager.widget.ViewPager/>
+
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(new MyFragmentAdapter(getSupportFragmentManager()));
     }
 }
