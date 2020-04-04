32cd24e47151b8ed7c6c9d2c6d4dcfde19cc5683
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-04-04 14:28:12 +0200

    cd..

--- Zajecia4/RetrofitExample/app/src/main/java/com/e/retrofitexample/MainActivity.java
+++ Zajecia4/RetrofitExample/app/src/main/java/com/e/retrofitexample/MainActivity.java
@@ -4,7 +4,13 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +19,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        OkHttpClient client = new OkHttpClient();
+        final OkHttpClient client = new OkHttpClient();
+        final Request request = new Request.Builder().url("http://wsi.ovh/json").build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+            }
+        });
+
+
     }
 }
