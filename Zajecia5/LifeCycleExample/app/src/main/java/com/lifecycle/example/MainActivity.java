ba786e18459e007d9ed6220a76a22e28d2032827
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-04-18 14:35:58 +0200

    cd..

--- Zajecia5/LifeCycleExample/app/src/main/java/com/lifecycle/example/MainActivity.java
+++ Zajecia5/LifeCycleExample/app/src/main/java/com/lifecycle/example/MainActivity.java
@@ -2,40 +2,50 @@ package com.lifecycle.example;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textViewValueA;
+    TextView textViewValueB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textViewValueA = findViewById(R.id.textViewA);
+        textViewValueB = findViewById(R.id.textViewB);
+
+        textViewValueA.setText("0");
+        textViewValueB.setText("0");
+
         DataViewModel dataViewModel = ViewModelProviders.of(this).get(DataViewModel.class);
 
-        Log.d("LifeCycle","OnCreate");
+        Log.d("LifeCycle", "OnCreate");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        Log.d("LifeCycle","OnStart");
+        Log.d("LifeCycle", "OnStart");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
 
-        Log.d("LifeCycle","OnStop");
+        Log.d("LifeCycle", "OnStop");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        Log.d("LifeCycle","OnResume");
+        Log.d("LifeCycle", "OnResume");
     }
 }
