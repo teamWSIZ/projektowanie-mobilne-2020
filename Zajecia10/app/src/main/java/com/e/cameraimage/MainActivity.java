bee3bd682ea6b5c53c75bee19621abe8cd45478f
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-09-12 14:21:15 +0200

    cd..

--- Zajecia10/app/src/main/java/com/e/cameraimage/MainActivity.java
+++ Zajecia10/app/src/main/java/com/e/cameraimage/MainActivity.java
@@ -1,14 +1,22 @@
 package com.e.cameraimage;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button cameraButton;
+    ImageView cameraImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cameraButton = findViewById(R.id.cameraButton);
+        cameraImage = findViewById(R.id.cameraImage);
     }
 }
