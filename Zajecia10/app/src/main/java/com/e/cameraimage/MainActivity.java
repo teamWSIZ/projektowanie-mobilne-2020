1f76e8ce05ba3644c537cd8922a1118a9230b132
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-09-12 14:28:11 +0200

    cd..

--- Zajecia10/app/src/main/java/com/e/cameraimage/MainActivity.java
+++ Zajecia10/app/src/main/java/com/e/cameraimage/MainActivity.java
@@ -1,15 +1,20 @@
 package com.e.cameraimage;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.mindorks.paracamera.Camera;
+
 public class MainActivity extends AppCompatActivity {
 
     Button cameraButton;
     ImageView cameraImage;
+
+    Camera camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +23,26 @@ public class MainActivity extends AppCompatActivity {
 
         cameraButton = findViewById(R.id.cameraButton);
         cameraImage = findViewById(R.id.cameraImage);
+
+        camera = new Camera.Builder()
+                .resetToCorrectOrientation(true)// it will rotate the camera bitmap to the correct orientation from meta data
+                .setTakePhotoRequestCode(1)
+                .setDirectory("pics")
+                .setName("ali_" + System.currentTimeMillis())
+                .setImageFormat(Camera.IMAGE_JPEG)
+                .setCompression(75)
+                .setImageHeight(1000)// it will try to achieve this height as close as possible maintaining the aspect ratio;
+                .build(this);
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    camera.takePicture();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
